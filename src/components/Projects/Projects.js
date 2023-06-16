fdecd0ce9e34184152fcf87198025014ec15d8aa
MAURYA SONI <88384389+maurya2201@users.noreply.github.com>
2023-06-16 18:38:05 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,13 +6,16 @@ import leaf from "../../Assets/5.webp";
 import emotion from "../../Assets/5.2.webp";
 import editor from "../../Assets/5.1.webp";
 import chatify from "../../Assets/1.png"
-import suicide from "../../Assets/6.9.jpeg";
+import suicide from "../../Assets/lms.png";
 import bitsOfCode from "../../Assets/2.png";
 
 
 
 
+
+
 function Projects() {
+
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -28,9 +31,14 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="COLLEGE WEBSITE"
-              description=<center>TECHNOLOGIES USED:-HTML,CSS </center>
-              ghLink="https://github.com/maurya2201/COLLEGE-WEBSITE"
+              title=<center>COLLEGE WEBSITE</center>
+              description={<center>------------TECHNOLOGIES USED---------------
+                <ul>
+                 <li>HTML,CSS(FRONTEND)</li> 
+                  </ul>
+                  </center>}
+                  ghLink="https://github.com/maurya2201/COLLEGE-WEBSITE"
+                  demoLink="https://maurya2201.github.io/COLLEGE-WEBSITE/"
             />
           </Col>
 
@@ -38,9 +46,14 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="TRAVEL WEBSITE"
-              description="TECHNOLOGIES USED:-HTML,CSS,JS,SCSS"
-              ghLink="https://github.com/maurya2201/TRAVEL-WEBSITE"
+              title=<center>TRAVEL WEBSITE</center>
+              description={<center>------------TECHNOLOGIES USED---------------
+                <ul>
+                 <li>HTML,CSS,JS(FRONTEND)</li> 
+                  </ul>
+                  </center>}
+                 ghLink="https://github.com/maurya2201/TRAVEL-WEBSITE"
+                 demoLink="https://maurya2201.github.io/TRAVEL-WEBSITE/"
             />
           </Col>
 
@@ -48,11 +61,17 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="BOOK RECOMMENDATION SYSTEM"
-              description="MODEL:-PYTHON FRONTEND:-HTML,CSS AND BOOTSTRAP BACKEND:-FLASK"
-              ghLink="https://github.com/maurya2201/BOOK-RECOMMENDATION-SYSTEM"
-           
+              title=<center>BOOK RECOMMENDATION SYSTEM</center>
+              description={<center>------------TECHNOLOGIES USED---------------
+                <ul>
+                 <li>HTML,CSS,JS,BOOTSTRAP(FRONTEND)</li> 
+                 <li>FLASK(BACKEND)</li> 
+                 <li>PYTHON(MODEL)</li>  
+                  </ul>
+                  </center>}
+                  ghLink="https://github.com/maurya2201/BOOK-RECOMMENDATION-SYSTEM"
             />
+            
           </Col>
 
           <Col md={4} className="project-card">
@@ -60,32 +79,55 @@ function Projects() {
               imgPath={leaf}
               isBlog={false}
               title=<center>E-COMMERCE-USING-REACT-JS</center>
-              description=<center>TECHNOLOGIES USED:-REACT-JS</center>
+              description={<center>------------TECHNOLOGIES USED---------------
+                <ul>
+                 <li>REACT(FRONTEND)</li> 
+                  </ul>
+                  </center>}
               ghLink="https://github.com/maurya2201/E-COMMERCE-USING-REACT-JS"
+              demoLink="https://fashionhubreactjs.netlify.app/"
+            />
+            
+          </Col>
+         <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title=<center>CHAT APPLICATION</center>
+              description={<center>------------TECHNOLOGIES USED---------------
+                <ul>
+                 <li>REACT(FRONTEND)</li> 
+                 <li>FIREBASE(DATABASE)</li> 
+                  </ul>
+                  </center>}
+              ghLink="https://github.com/maurya2201/CHAT-APPLICATION"
+              demoLink="https://chatapprnf.netlify.app/"
             />
           </Col>
           <Col md={4} className="project-card">
           <ProjectCard
             imgPath={suicide}
             isBlog={false}
-            title="PLANT APP"
-            description={<center>TECHNOLOGIES USED:- ANDROID(JAVA)</center>}
-            ghLink="https://github.com/maurya2201/PLANT-APP.git"
+            title=<center>LIBRARY MANAGEMENT SYSTEM</center>
+            description={<center>------------TECHNOLOGIES USED---------------
+            <ul>
+             <li>REACT(FRONTEND)</li> 
+             <li>NODE(BACKEND)</li> 
+             <li>MONGODB(DATABASE)</li> 
+              </ul>
+              </center>}
+            ghLink="https://github.com/maurya2201/LIBRARY-MANAGEMENT-SYSTEM-MERN"
+           
           />
-        </Col>        
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title=<center>CHAT APPLICATION ON EC2</center>
-              description=<center>USED:-REACT.JS,FIREBASE AND AWS</center>
-              ghLink="https://github.com/maurya2201/CHAT-APPLICATION"
-            />
-          </Col>
+        </Col> 
         </Row>
       </Container>
+     
     </Container>
+
+    
+  
+
   );
 }
 
